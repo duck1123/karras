15f8d194386d05dd6e2532c3938475ebb1a79db9
Daniel E. Renfer <duck@kronkltd.net>
2011-09-22 19:06:52 -0400

    kronkltd

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject karras "0.8.1-SNAPSHOT"
+(defproject net.kronkltd/karras "0.8.1-SNAPSHOT"
   :description "A clojure entity framework for MongoDB"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.mongodb/mongo-java-driver "2.7.2"]
