ea3418447bcea920fd218a48c22bf08f5e697e63
Daniel E. Renfer <duck@kronkltd.net>
2011-12-10 13:29:43 -0500

    update midje

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
                  [org.clojure/clojure-contrib "1.2.0"]
                  [org.mongodb/mongo-java-driver "2.7.2"]
                  [inflections "0.6.3"]]
-  :dev-dependencies [[midje "1.3.0-RC4"]]
+  :dev-dependencies [[midje "1.3-alpha6" :exclusions [org.clojure/clojure]]]
   :autodoc {:web-src-dir "http://github.com/wilkes/karras/blob/"
             :web-home "http://wilkes.github.com/karras"})
