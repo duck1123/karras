434273b00591d41eda9164f5db2c5a408718e69c
Daniel E. Renfer <duck@kronkltd.net>
2011-12-10 14:49:24 -0500

    update to clojure 1.3

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject karras "0.8.1-SNAPSHOT"
   :description "A clojure entity framework for MongoDB"
-  :dependencies [[org.clojure/clojure "1.2.0"]
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.mongodb/mongo-java-driver "2.7.2"]
                  [inflections "0.6.3"]]
   :dev-dependencies [[midje "1.3-alpha6" :exclusions [org.clojure/clojure]]]
